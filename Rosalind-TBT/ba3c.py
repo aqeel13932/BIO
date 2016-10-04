b15f1e9f2f3a58a616353e89ebc8243bad90eb20
aqeel13932 <aqeel.labash@gmail.com>
2016-10-04 07:06:29 +0300

    add ba3c

---
+++ Rosalind-TBT/ba3c.py
@@ -0,0 +1,21 @@
+def Overlap(Patterns):
+    out =[]
+    lookup = {}
+    for pat in Patterns:
+        lookup[pat[:-1]]=pat
+    for pat in Patterns:
+        if pat[1:] in lookup.keys():
+            out.append('{} -> {}'.format(pat,lookup[pat[1:]]))
+    return out
+
+if __name__ == "__main__":
+    with open('data') as f:
+        dna_lst = map(lambda x: x.strip(),f.readlines())
+
+    ls = Overlap(dna_lst)
+    ls.sort()
+    print '\n'.join(ls)
+    with open('log.out','w') as f:
+        f.write('\n'.join(ls))
+
+
