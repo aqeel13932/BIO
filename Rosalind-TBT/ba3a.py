d6eff2726f226e3431475801e73b7a96b2c69eef
aqeel13932 <aqeel.labash@gmail.com>
2016-10-04 06:46:31 +0300

    solve ba3a , ba3b

---
+++ Rosalind-TBT/ba3a.py
@@ -0,0 +1,16 @@
+def Composition(text,k):
+    ls=[]
+    for i in range(len(text)-k+1):
+        ls.append(text[i:i+k])
+    ls.sort()
+    return ls
+
+if __name__ == "__main__":
+    with open('data') as f:
+        k = int(f.readline().strip())
+        dna = f.readline().strip()
+    ls = Composition(dna,k)
+    with open('log.out','w') as f:
+        f.write('\n'.join(ls))
+
+
---
+++ Rosalind-TBT/ba3b.py
@@ -0,0 +1,16 @@
+def ReconstructString(lst):
+    out = lst[0]
+    k = len(lst[0])
+    for j in range(1,len(lst)):
+        out+=lst[j][k-1]
+    return out
+
+if __name__ == "__main__":
+    with open('data') as f:
+        dna_lst = map(lambda x: x.strip(),f.readlines())
+    ls = ReconstructString(dna_lst)
+    print ls
+    with open('log.out','w') as f:
+        f.write(ls)
+
+
