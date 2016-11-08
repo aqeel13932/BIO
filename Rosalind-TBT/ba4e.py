0c937ac128af816d8024d2bdedce52bee4666d02
aqeel13932 <aqeel.labash@gmail.com>
2016-11-08 15:07:17 +0200

    finished ba4e

--- Rosalind-TBT/ba4e.py
+++ Rosalind-TBT/ba4e.py
@@ -1,35 +1,73 @@
-from itertools import cycle,islice
-values={'G':57,'A':71,'S':87,'P':97,'V':99,'T':101,'C':103,'I':113,'L':113,'N':114,'D':115,'K':128,'Q':128,'E':129,'M':131,'H':137,'F':147,'R':156,'Y':163,'W':186}
+from itertools import permutations
+from ba4c import GENERATE_THEORETICAL_SPECTRUM
+values={'G':57,'A':71,'S':87,'P':97,'V':99,'T':101,'C':103,'I':113,'N':114,'D':115,'K':128,'E':129,'M':131,'H':137,'F':147,'R':156,'Y':163,'W':186}
 def GET_TOTAL(peptidestr):
     total = 0
     for l in peptidestr:
         total+=values[l]
     return total
 
-def GENERATE_THEORETICAL_SPECTRUM(peptide):
-    peptides=[]
-    values=[]
-    for i in range (1,len(peptide)):
-        for j in range(len(peptide)):
-            c = cycle(peptide)
-            val=list( islice(c,j,j+i))
-            peptides.append(''.join(val))
-            values.append(GET_TOTAL(val))
-    peptides.append(peptide)
-    values.append(GET_TOTAL(peptide))
-    values.append(0)
-    return values
+def CONSISTANT(pep,PEPVALS):
+    if GET_TOTAL(pep) not in PEPVALS:
+        return False
+    for i in range(1,len(pep)):
+        for j in range(len(pep)-i+1):
+            if GET_TOTAL(pep[j:j+i]) in PEPVALS:
+                continue
+            else:
+                return False
+    return True
 
+def LIST_IN_LIST(pep,PEPVALS):
+    therom = GENERATE_THEORETICAL_SPECTRUM(pep)
+    for j in therom:
+        if j in PEPVALS:
+            continue
+        else:
+            print j
+            return False
+    return True
 
+def TRIM(PEPTIDES,PEPVALS):
+    nlst =[]
+    for pep in PEPTIDES:
+        if CONSISTANT(pep,PEPVALS):
+        #if LIST_IN_LIST(pep,PEPVALS):
+            nlst.append(pep)
+    nlst = set(nlst)
+    nlst = list(nlst)
+    return nlst
+def EXPAND(lst,base):
+    expand =[]
+    for i in range(len(lst)):
+        for v in base:
+            expand.append(lst[i]+v)
+    return expand
+
+def FIND_CYCLYIC_PEPTIDE(PEPVALS):
+    for i in range(len(PEPVALS)):
+        if sum(PEPVALS[:i])==PEPVALS[-1]:
+            break
+    baseline = []
+    for v in values:
+        if values[v] in PEPVALS:
+            baseline.append(v)
+    expand = EXPAND(baseline,baseline)
+    trimed = TRIM(expand,PEPVALS)
+    while len(trimed[0])!=(i-1):
+        print len(trimed[0]),(i-1)
+        expand = EXPAND(trimed,baseline)
+        trimed=TRIM(expand,PEPVALS)
+    return trimed
+def PEPTIDE_TO_NUMBER(pep):
+    return '-'.join(map(lambda x:str(values[x]),pep))
 if __name__ == "__main__":
     
     with open('data') as f:
-        peptidecode= f.readline().strip()
-    res = GET_TOTAL(peptidecode)
-    res = GENERATE_THEORETICAL_SPECTRUM(peptidecode)
-    res.sort()
-    print GET_TOTAL('LN')
-    print res
+        pepvals = map(int,f.readline().strip().split())
+    res = FIND_CYCLYIC_PEPTIDE(pepvals)
+    res = map(PEPTIDE_TO_NUMBER,res)
+    #print res
     with open('log.txt','w') as f:
         for i in res:
             f.write('{} '.format(i))
